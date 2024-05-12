9636357177f417bc92cce87dd5d8f649547a349e
bibek0923 <115898856+bibek0923@users.noreply.github.com>
2024-05-12 05:23:30 +0000

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,29 @@
+const name="jack"
+let age=16
+var address="LA"
+phone_num=999838383
+
+console.log(name);
+console.log(address);
+console.log(age);
+console.log(phone_num);
+
+
+// name="paul" const values can't be changed
+age=18
+address="NYC"
+phone_num=999999 
+
+console.log(address);
+console.log(age);
+console.log(phone_num);
+
+// console.table([name,phone_num,address,age]) shortcut for displaying multiple things in console
+
+
+
+// let is widely used than that of var 
+/* 
+prefer not to use var
+because of issue in block scope and functional scope
+*/
