0f6a35097af38b3128c2b221d4bdcd984155ddf8
bibek0923 <115898856+bibek0923@users.noreply.github.com>
2024-05-15 19:45:32 +0000

    datatypes in js

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict" //treats all js code as a newer version
+
+let age=18
+let name="hero"
+
+
+
+
+
+
+//typeof
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof({a:23}))
+console.log(typeof null);
+console.log(typeof undefined);
